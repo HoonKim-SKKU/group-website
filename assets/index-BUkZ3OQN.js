c78c1c11804e531b3f8d9b39d0f4be4e8c1f10ec
Hoon Kim <hoonkim@Hoons-MacBook-Pro-213.local>
2026-04-17 08:16:34 +0900

    Updates

--- assets/index-BUkZ3OQN.js
+++ assets/index-BUkZ3OQN.js
@@ -121,7 +121,7 @@ Please change the parent <Route path="${K}"> to <Route path="${K==="/"?"*":`${K}
           align-items: center;
           width: 100%;
           position: relative;
-          padding-bottom: 6rem; 
+          padding-bottom: 8rem; 
           z-index: 1;
         }
 
